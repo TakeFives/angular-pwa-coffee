9e0e97d77ab104e89e2d6d9f4950d6a548b2cf4e
Dashunya <dashunya@MacBook-Pro-NMS.local>
2024-03-22 22:52:19 -0600

    feat: add geolocation service

---
+++ src/app/geolocation.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { buffer } from 'rxjs';
+import { PlaceLocation } from './logic/PlaceLocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocationService {
+
+  requestLocation(callback: Function) {
+    // W3C Geolocation API
+
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        callback(position.coords)
+      },
+      error => {
+        // TODO: log error to the window
+        callback(null)
+      }
+    )
+  }
+
+  getMapLink(location: PlaceLocation) {
+    let query = '';
+    if (location.latitude && location.longtitude) {
+      query = `${location.latitude},${location.longtitude}`;
+    } else {
+      query = `${location.adress},${location.city}`;
+    }
+
+    if(/iPad|iPhone|iPod/.test(navigator.userAgent)){
+      return `https://maps.apple.com/?q=${query}`;
+    }else{
+      return `https://maps.google.com/?q=${query}`;
+    }
+  }
+  // Universal links
+
+  constructor() { }
+}
