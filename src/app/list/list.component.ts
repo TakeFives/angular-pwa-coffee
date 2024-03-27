819e5ad375982ab84c7cf836923c6c02f43ea96a
Dashunya <dashunya@MacBook-Pro-NMS.local>
2024-03-27 13:15:56 -0600

    feat: add share handler

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -11,29 +11,40 @@ import { GeolocationService } from '../geolocation.service';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
-    list:  Coffee[] = [];
+  list: Coffee[] = [];
 
-    constructor(
-      private data:DataService,
-      private router:Router,
-      private geolocationService: GeolocationService,
-    ){}
+  constructor(
+    private data: DataService,
+    private router: Router,
+    private geolocationService: GeolocationService,
+  ) { }
 
-    goDetails(coffee:Coffee){
-      this.router.navigate(["/coffee", coffee._id]);
+  goDetails(coffee: Coffee) {
+    this.router.navigate(["/coffee", coffee._id]);
+  }
+
+  goMap(coffee: Coffee) {
+    const mapUrl = this.geolocationService.getMapLink(coffee.location!);
+    // location.href = mapUrl;
+    window.open(mapUrl, "_blank");
+  };
+
+  share(coffee: Coffee) {
+    const shareText = `I had this coffee at ${coffee.place} and for me it's ${coffee.rating} stars.`
+    const shareInfo = {
+      title: coffee.name,
+      text: shareText,
+      url: window.location.href
     }
 
-    goMap(coffee:Coffee){
-      const mapUrl  = this.geolocationService.getMapLink(coffee.location!);
-      // location.href = mapUrl;
-      window.open(mapUrl, "_blank");
-    };
-
-    share(coffee:Coffee){};
-
-    ngOnInit(){
-      this.data.getList((list: Coffee[]) => {
-        this.list = list
-      })
+    if ('share' in Navigator && navigator.canShare(shareInfo)) {
+      navigator.share(shareInfo)
     }
+  };
+
+  ngOnInit() {
+    this.data.getList((list: Coffee[]) => {
+      this.list = list
+    })
+  }
 }
